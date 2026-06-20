bbce67e3ced9387eb51c93d1c3c1bbc6ac09baa7
Pepa <9425683+pepa667@users.noreply.github.com>
2026-06-20 13:50:29 -0300

    Testando melhoria de performance

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
+import Unfonts from 'unplugin-fonts/vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    Unfonts({
+      google: {
+        families: [
+          {
+            name: "Roboto Slab",
+            styles: "wght@400;700;900",
+          },
+          {
+            name: "Inter",
+            styles: "wght@300;400;600",
+          },
+          {
+            name: "Space Mono",
+          }
+        ],
+        display: 'swap',
+      },
+    }),
   ],
 })
